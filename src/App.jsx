7fca52ce34bbdb64173fbcc0e6b23f3470a8cb73
shalini J <shalinijagan195@gmail.com>
2026-02-04 16:13:51 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Hero from "./components/hero";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Education from "./components/Education";
+import Internship from "./components/Internship";
+import Certifications from "./components/Certifications";
+import Contact from "./components/Contact";
+
+function App() {
+  return (
+    <>
+      <Hero />
+      <Skills />
+      <Projects />
+      <Education />
+      <Internship />
+      <Certifications />
+      <Contact />
+    </>
+  );
+}
+
+export default App;
