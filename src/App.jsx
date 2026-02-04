b7a37fc003730a95a952befb05b9bd9ef1384d39
shalini J <shalinijagan195@gmail.com>
2026-02-04 17:00:13 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Hero from "./components/hero";
+import Hero from "./components/Hero";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Education from "./components/Education";
